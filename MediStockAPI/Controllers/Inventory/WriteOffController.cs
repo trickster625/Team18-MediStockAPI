6ac18017065ce671de987c22e42ac1bc31e6cff1
FrancoBreugem <francobreugem@gmail.com>
2021-08-13 10:30:52 +0200

    Inventory 90%

---
+++ MediStockAPI/Controllers/Inventory/WriteOffController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using MediStockAPI.Models;
+
+namespace MediStockAPI.Controllers.Inventory
+{
+    public class WriteOffController : ApiController
+    {
+
+        MediStock_DBEntities db = new MediStock_DBEntities();
+
+        [HttpPost]
+        [Route("createWriteOff")]
+        public IHttpActionResult createWriteOff(Written_OffInventory writeOff)
+        {
+            try
+            {
+                Models.Inventory currentItem = new Models.Inventory();
+                currentItem = db.Inventories.Where(z => z.Inventory_ID == writeOff.Inventory_ID).FirstOrDefault();
+                currentItem.Inventory_BaseCampQty = (currentItem.Inventory_BaseCampQty - writeOff.Write_Off_Qty);
+
+                db.Written_OffInventory.Add(writeOff);
+
+                db.SaveChangesAsync();
+
+                return Ok("WriteOff Added");
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+
+    }
+}
