cd71ac3455b3d39f4afa0510b457551f3bbd7f02
Tyla Brown <56531004+tb-creator@users.noreply.github.com>
2021-08-22 16:04:26 +0200

    Models / Controllers - Tyla Commit

---
+++ MediStockAPI/Models/RegisterEmployee.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MediStockAPI.Models
+{
+    public class RegisterEmployee
+    {
+        public int Employee_ID { get; set; }
+
+        public int EmployeeTitle_ID { get; set; }
+
+        public int SecurityQuestion_ID { get; set; }
+
+        public string Employee_Name { get; set; }
+
+        public string Employee_Surname { get; set; }
+
+        public string Employee_IDNumber { get; set; }
+
+        public string Employee_ContactNumber { get; set; }
+
+        public string Employee_EmailAddress { get; set; }
+
+        public string Employee_HashedPassword { get; set; }
+
+        public string Employee_SecurityQuestionAnswer { get; set; }
+
+        public string Employee_SARSTaxNumber { get; set; }
+
+        public byte[] Employee_Signature { get; set; }
+    }
+}
