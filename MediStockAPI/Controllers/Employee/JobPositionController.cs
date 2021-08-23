d4fcbf8bf90ebbbf6aa3a373f1d23f60e848a07d
Tyla Brown <56531004+tb-creator@users.noreply.github.com>
2021-08-23 10:09:05 +0200

    Controllers - Tyla Commit

---
+++ MediStockAPI/Controllers/Employee/JobPositionController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using MediStockAPI.Models;
+
+namespace MediStockAPI.Controllers
+{
+    public class JobPositionController : ApiController
+    {
+        MediStock_DBEntities db = new MediStock_DBEntities();
+
+        [HttpGet]
+        [Route("getJobPositions")]
+        public IHttpActionResult getJobPositions()
+        {
+            try
+            {
+                List<JobPosition> outputJobPosition = new List<JobPosition>();
+                var storedJobPosition = db.JobPosition.ToList();
+
+                foreach (var storedJobPosition in storedJobPositions)
+                {
+                    JobPosition position = new JobPosition();
+                    position.JobPosition_ID = storedJobPosition.JobPosition_ID;
+                    position.JobPosition_Description = storedJobPosition.JobPosition_Description;
+                    outputJobPosition.Add(position);
+                }
+
+                return Ok(outputJobPosition);
+            }
+            catch (Exception)
+            {
+
+                return BadRequest();
+            }
+        }
+    }
+}
