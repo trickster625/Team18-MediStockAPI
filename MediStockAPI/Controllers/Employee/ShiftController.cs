6abfdf168247a8c30e6d08e2e4a1313dfbb5dda3
Tyla Brown <56531004+tb-creator@users.noreply.github.com>
2021-08-29 09:33:41 +0200

    basic shift started - tyla

---
+++ MediStockAPI/Controllers/Employee/ShiftController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MediStockAPI.Controllers.Employee
+{
+    public class ShiftController : ApiController
+    {
+        MediStock_DBEntities db = new MediStock_DBEntities();
+
+        [HttpGet]
+        [Route("getEmployeeShifts")]
+
+        public IHttpActionResult getEmployeeShifts()
+        {
+            try
+            {
+                List<Shift> outputEmployeeShift = new List<Shift>();
+
+                var storedEmployeeShifts = db.Shift.ToList();
+                
+                foreach (var storedEmployeeShifts in storedEmployeeShift)
+                {
+                    Shift shift = new Shift();
+
+                    shift.Shift_ID = storedEmployeeShifts.Shift_ID;
+                    shift.Employee_ID = storedEmployeeShifts.Employee_ID;
+                    shift.ShiftDate = storedEmployeeShifts.ShiftDate;
+                    shift.ShiftStartTime = storedEmployeeShifts.ShiftStartTime;
+                    shift.ShiftEndTime = storedEmployeeShifts.ShiftEndTime;
+
+                    outputEmployeeShift.Add(shift);
+                }
+
+                return Ok(outputEmployeeShift);
+
+            }
+            catch (Exception)
+            {
+
+                return BadRequest();
+            }
+        }
+
+        [HttpGet]
+        [Route("getFilteredEmployeeShifs")]
+        public IHttpActionResult getFilteredEmployeeShifts(string searchEmployee_Name, int searchShiftDate)
+        {
+
+            try
+            {
+                var EmployeeShifts = db.Shift.ToList();
+
+                List<Shift> EmployeeShiftList = new List<Shift>();
+
+                foreach (var storedEmployees in employee)
+                {
+
+                    if (EmployeeShifts.Employee_Name == searchEmployee_Name && EmployeeShifts.ShiftDate == searchShiftDate)
+                    {
+                        Shift shift = new Shift();
+
+                        shift.Shift_ID = storedEmployeeShifts.Shift_ID;
+                        shift.Employee_ID = storedEmployeeShifts.Employee_ID;
+                        shift.ShiftDate = storedEmployeeShifts.ShiftDate;
+                        shift.ShiftStartTime = storedEmployeeShifts.ShiftStartTime;
+                        shift.ShiftEndTime = storedEmployeeShifts.ShiftEndTime;
+
+                        EmployeeShiftList.Add(shift);
+                    }
+
+                }
+
+                return Ok(EmployeeShiftsList);
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+
+
+        [HttpPost]
+        [Route("createShift")]
+        public IHttpActionResult createShift(CreateShift newShift)
+        {
+            try
+            {
+                db.Shift.Add(newShift);
+                db.SaveChangesAsync();
+
+                return Ok("Shift Added!");
+            }
+            catch (Exception)
+            {
+
+                return BadRequest();
+            }
+        }
+
+    }
+}
---
+++ MediStockAPI/Models/Shift.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MediStockAPI.Models
+{
+    public class Shifts
+    {
+        public int Shift_ID { get; set; }
+
+        public int Employee_ID { get; set; }
+
+        public int ShiftDate { get; set; }
+
+        public string ShiftStartTime { get; set; }
+
+        public string ShiftEndTime { get; set; }
+
+    }
+}
